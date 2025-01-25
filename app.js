52aeb9ea322467ee4a8c158e95bb85e2e7b24610
HarshMane2604 <maneharsh72@gmail.com>
2025-01-25 20:22:18 +0530

    The main logic of game is creted

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
 let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
-choices.forEach((choice) => {
-    console.log(choice);
+
+const genCompChoice = () => {
+    let options = ["rock", "paper", "scissors"]; 
+    return options[Math.floor(Math.random() * options.length)];   
+    // rock, paper, Scissor
+}
+
+const drawGame = () => {
+    console.log("Game was draw");
     
+}
+
+const showWinner = (userWin) => {
+    if(userWin){
+        console.log("You Win!");
+    }else{
+        console.log("You Loose!");
+    }
+    
+}
+const playGame = (userChoice) => {
+    console.log("User Choice = ", userChoice);
+    const compChoice = genCompChoice();
+    console.log("Comp Choice = ", compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else{
+     let userWin = true;
+     if(userChoice === "rock"){
+        // Scissors, paper
+        userWin = compChoice === "paper" ? false : true; 
+     } else if(userChoice === "paper"){
+        // rock, scissor
+        userWin = compChoice === "scissors" ? false : true; 
+     } else {
+        // paper, rock
+        userWin = compChoice === "rock" ? false : true; 
+     }
+    showWinner(userWin);
+    }
+}
+
+
+choices.forEach((choice) => {    
     choice.addEventListener("click", () => {
-        console.log("choice was clicked");
-        
+        const userChoice = choice.getAttribute("id");
+        // console.log("choice was clicked", userChoice);
+        playGame(userChoice)  
     })
 })
 
